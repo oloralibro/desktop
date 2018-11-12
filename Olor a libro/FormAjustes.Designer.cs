3f1ea157562d7684ef59504781a1d110723c04d7
Jorge Sereno <jorge@hotmail.com>
2018-11-12 22:24:29 +0100

    prueba sincronizar

--- Olor a libro/FormAjustes.Designer.cs
+++ Olor a libro/FormAjustes.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonEditarIcono = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonEditarIcono
+            // 
+            this.buttonEditarIcono.Location = new System.Drawing.Point(25, 179);
+            this.buttonEditarIcono.Name = "buttonEditarIcono";
+            this.buttonEditarIcono.Size = new System.Drawing.Size(128, 23);
+            this.buttonEditarIcono.TabIndex = 1;
+            this.buttonEditarIcono.Text = "Editar icono";
+            this.buttonEditarIcono.UseVisualStyleBackColor = true;
             // 
             // FormAjustes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(197)))), ((int)(((byte)(220)))));
-            this.ClientSize = new System.Drawing.Size(423, 287);
+            this.ClientSize = new System.Drawing.Size(523, 323);
+            this.Controls.Add(this.buttonEditarIcono);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "FormAjustes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Olor A Libro - Ajustes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonEditarIcono;
     }
 }
